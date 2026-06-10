e982fc7a930ee217d39bfd96c84c717ab3ae00aa
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-06-10 20:58:00 +0700

    add base component

---
+++ src/components/base/BaseAlert.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+  <div
+    class="flex items-center gap-2 px-4 py-3 text-red-700 bg-red-50 border border-red-100 rounded-xl dark:bg-red-950 dark:border-red-900 dark:text-red-300"
+  >
+    <Icon icon="ri:alert-fill" class="size-4" />
+    <slot />
+  </div>
+</template>
--- src/components/base/BaseInput.vue
+++ src/components/base/BaseInput.vue
@@ -1,5 +1,10 @@
+<script setup>
+const value = defineModel();
+</script>
+
 <template>
   <input
     class="h-13 px-5 w-full border border-transparent rounded-2xl bg-neutral-50 transition placeholder:text-neutral-400 focus:bg-white focus:outline-none focus:border-neutral-300 dark:bg-neutral-800 dark:placeholder:text-neutral-500 dark:focus:bg-neutral-800 dark:focus:border-neutral-600 dark:text-white"
+    v-model="value"
   />
 </template>
