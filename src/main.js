f3facb6a7554e23b7d1adcf7d657000541bd7e55
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-06-11 19:59:32 +0700

    dropdown component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import './style.css';
+import 'floating-vue/dist/style.css';
 import App from './App.vue';
 import vClickOutside from 'click-outside-vue3';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import FloatingVue from 'floating-vue';
 import { router } from './core/router/router';
 import { layouts } from './components/layouts';
 
@@ -17,6 +19,7 @@ for (const layout in layouts) {
   app.component(layout, layouts[layout]);
 }
 
+app.use(FloatingVue);
 app.use(router);
 app.use(pinia);
 app.use(vClickOutside);
