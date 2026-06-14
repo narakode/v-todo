50fb89cc10f1ebb44d654cac96aab966461675cc
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-06-14 16:14:27 +0700

    toc and privacy page

---
+++ src/common/pages/404.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BaseState from '../../components/base/BaseState.vue';
+</script>
+
+<template>
+  <BaseState
+    icon="ri:folder-close-line"
+    title="404 Not Found"
+    description="Halaman yang anda cari tidak ditemukan"
+  />
+</template>
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,24 @@ export const routes = [
     },
   },
   {
+    path: '/privacy',
+    name: 'privacy',
+    component: () => import('./common/pages/Privacy.vue'),
+    meta: {
+      title: 'Privacy Policy',
+      layout: 'LayoutPage',
+    },
+  },
+  {
+    path: '/terms-of-service',
+    name: 'terms-of-service',
+    component: () => import('./common/pages/TermsOfService.vue'),
+    meta: {
+      title: 'Terms Of Service',
+      layout: 'LayoutPage',
+    },
+  },
+  {
     path: '/login',
     name: 'login',
     component: () => import('./features/auth/pages/LoginPage.vue'),
@@ -26,6 +44,15 @@ export const routes = [
       guest: true,
       layout: 'LayoutAuth',
       title: 'Register',
+    },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('./common/pages/404.vue'),
+    meta: {
+      title: '404 Not Found',
+      layout: 'LayoutPage',
     },
   },
 ];
